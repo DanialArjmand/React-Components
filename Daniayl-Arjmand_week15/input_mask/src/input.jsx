b10c8db35f32a3841e6bc71032908dbb95aeaad4
Danial <127108898+DanialArjmand@users.noreply.github.com>
2025-07-14 19:43:26 +0330

    Add Icon and handleClear

--- Daniayl-Arjmand_week15/input_mask/src/input.jsx
+++ Daniayl-Arjmand_week15/input_mask/src/input.jsx
@@ -1,13 +1,29 @@
-/* eslint-disable react/prop-types */
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 
 const Input = ({ value, handleChange, ghost }) => {
+  const showGhost =
+    ghost.toLowerCase().startsWith(value.toLowerCase()) &&
+    ghost.length > value.length;
+
+  const handleClear = () => {
+    handleChange({ target: { value: "" } });
+  };
+
   return (
     <div className="input">
+      <div className="search-icon">
+        <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" color="#aaa" />
+      </div>
+
       <div className="ghost-text">
         <span>{value}</span>
-        <span className="ghost-suggestion">{ghost.slice(value.length)}</span>
+        <span className="ghost-suggestion">
+          {showGhost ? ghost.slice(value.length) : ""}
+        </span>
       </div>
+
       <input
         type="text"
         value={value}
@@ -15,6 +31,12 @@ const Input = ({ value, handleChange, ghost }) => {
         autoComplete="off"
         placeholder="Search..."
       />
+
+      {value && (
+        <div className="clear-icon" onClick={handleClear}>
+          <FontAwesomeIcon icon={faXmark} size="lg" color="#666" />
+        </div>
+      )}
     </div>
   );
 };
