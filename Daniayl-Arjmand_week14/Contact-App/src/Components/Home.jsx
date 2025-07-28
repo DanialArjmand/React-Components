ac03ace6c8d3efea0d7b632a1e82cd6d694f8af3
Danial <127108898+DanialArjmand@users.noreply.github.com>
2025-07-28 14:27:46 +0330

    Add Text D.A

--- Daniayl-Arjmand_week14/Contact-App/src/Components/Home.jsx
+++ Daniayl-Arjmand_week14/Contact-App/src/Components/Home.jsx
@@ -81,58 +81,62 @@ const Home = () => {
   };
 
   return (
-    <div className={`home-root ${formVisible ? "visible" : ""}`}>
-      {view === "home" ? (
-        <>
-          <div className="art-container">
-            <Inputs
-              onSave={saveContactHandler}
-              onClose={toggleForm}
-              contactEdit={contactEdit}
-            />
-          </div>
-          <div className="glass-overlay"></div>
+    <div className="home-wrapper">
+      <div className={`home-root ${formVisible ? "visible" : ""}`}>
+        {view === "home" ? (
+          <>
+            <div className="art-container">
+              <Inputs
+                onSave={saveContactHandler}
+                onClose={toggleForm}
+                contactEdit={contactEdit}
+              />
+            </div>
+            <div className="glass-overlay"></div>
 
-          <div className="text-container">
-            <div className="button-container">
-              <button className="text-butt-user" onClick={toggleForm}>
-                <span className="label-butt"> افزودن </span>
-                <FontAwesomeIcon icon={faUserPlus} className="icon-user" />
-              </button>
+            <div className="text-container">
+              <div className="button-container">
+                <button className="text-butt-user" onClick={toggleForm}>
+                  <span className="label-butt"> افزودن </span>
+                  <FontAwesomeIcon icon={faUserPlus} className="icon-user" />
+                </button>
 
-              <button className="text-butt-list" onClick={showListPage}>
-                <span className="label-butt"> لیست مخاطبین </span>
-                <FontAwesomeIcon icon={faAddressBook} className="icon-list" />
-              </button>
+                <button className="text-butt-list" onClick={showListPage}>
+                  <span className="label-butt"> لیست مخاطبین </span>
+                  <FontAwesomeIcon icon={faAddressBook} className="icon-list" />
+                </button>
 
-              {/* <button className="text-butt-mode" onClick={toggleDarkMode}>
+                {/* <button className="text-butt-mode" onClick={toggleDarkMode}>
                 <span className="label-butt"> حالت شب</span>
                 <FontAwesomeIcon icon={faMoon} className="icon-moon" />
               </button> */}
+              </div>
+              <div className="text-content">
+                <h1>
+                  برنامه مخاطبین برای ثبت شماره و ایمیل های
+                  <span> تحت وب</span>
+                </h1>
+                <p>
+                  شما میتوانید با دکمه افزودن لیستی از مخاطبین را ثبت کنید و در
+                  هر فیلد اسم ، شماره تلفن ، ایمیل ، دسته بندی و جنسیت رو داشته
+                  باشید
+                </p>
+              </div>
             </div>
-            <div className="text-content">
-              <h1>
-                برنامه مخاطبین برای ثبت شماره و ایمیل های
-                <span> تحت وب</span>
-              </h1>
-              <p>
-                شما میتوانید با دکمه افزودن لیستی از مخاطبین را ثبت کنید و در هر
-                فیلد اسم ، شماره تلفن ، ایمیل ، دسته بندی و جنسیت رو داشته باشید
-              </p>
-            </div>
+          </>
+        ) : (
+          <div className="contact-list-page">
+            <ContactList
+              contacts={contacts}
+              onBack={showHomePage}
+              onEdit={startEditHandler}
+              onDelete={deleteContactHandler}
+              onDeleteMultiple={deleteMultipleContactsHandler}
+            />
           </div>
-        </>
-      ) : (
-        <div className="contact-list-page">
-          <ContactList
-            contacts={contacts}
-            onBack={showHomePage}
-            onEdit={startEditHandler}
-            onDelete={deleteContactHandler}
-            onDeleteMultiple={deleteMultipleContactsHandler}
-          />
-        </div>
-      )}
+        )}
+      </div>
+      <p className="footer-text">Developed by Daniyal.A | Bootstart </p>
     </div>
   );
 };
