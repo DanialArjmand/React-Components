4d1b82d30291d50f7a5d82b6794b73f4237ef355
Danial <127108898+DanialArjmand@users.noreply.github.com>
2025-08-15 18:36:06 +0330

    Customized Folder

---
+++ Daniyal-Arjmand_week19/Admin-panel/FrontEnd/src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+function LoginPage() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h2>فرم ورود</h2>
+      <input type="text" />
+      <input type="password" />
+      <button>ورود</button>
+      <a href="">ایجاد حساب کاربری</a>
+    </div>
+  );
+}
+
+export default LoginPage;
